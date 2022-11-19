5c4a7215f92ea723df292491bc3ee0670292373c
anik-kumaar-datta <anikkumaardatta@gmail.com>
2022-11-20 02:25:36 +0600

    first commit - Navbar added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCdhunuHOTnO5xtWHsOw9nsNnB3hxGUM7E",
+  authDomain: "pastry-cloud.firebaseapp.com",
+  projectId: "pastry-cloud",
+  storageBucket: "pastry-cloud.appspot.com",
+  messagingSenderId: "260617452340",
+  appId: "1:260617452340:web:32cfbb4685b4cbfebc0361",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
