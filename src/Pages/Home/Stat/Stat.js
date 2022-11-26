ce43bdb2e2a936c1673cb780a88df2a238690835
anik-kumaar-datta <anikkumaardatta@gmail.com>
2022-11-26 12:20:44 +0600

    15th commit - stat added

--- src/Pages/Home/Stat/Stat.js
+++ src/Pages/Home/Stat/Stat.js
@@ -6,8 +6,8 @@ const Stat = () => {
   console.log(user);
   return (
     <div className="max-w-6xl mx-auto text-center">
-      <div className="stats shadow mb-20">
-        <div className="stat">
+      <div className="shadow-lg mb-20 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 rounded-2xl">
+        <div className="my-6">
           <div className="stat-figure text-primary">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -22,13 +22,13 @@ const Stat = () => {
                 d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
               ></path>
             </svg>
+            <div className="stat-title">Total Reviews </div>
+            <div className="stat-value text-primary">25.6K</div>
+            <div className="stat-desc">21% more than last month</div>
           </div>
-          <div className="stat-title">Total Reviews </div>
-          <div className="stat-value text-primary">25.6K</div>
-          <div className="stat-desc">21% more than last month</div>
         </div>
 
-        <div className="stat">
+        <div className="my-6">
           <div className="stat-figure text-secondary">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -50,7 +50,7 @@ const Stat = () => {
         </div>
 
         {user ? (
-          <div className="stat">
+          <div className="my-6">
             <div className="stat-figure text-secondary">
               <div className="avatar online">
                 <div className="w-16 rounded-full">
@@ -63,7 +63,13 @@ const Stat = () => {
             <div className="stat-desc text-secondary">31 tasks remaining</div>
           </div>
         ) : (
-          <></>
+          <>
+            <div className="my-6">
+              <div className="stat-title">Page Visit</div>
+              <div className="stat-value text-slate-600">89.8K</div>
+              <div className="stat-desc">21% more than last month</div>
+            </div>
+          </>
         )}
       </div>
     </div>
